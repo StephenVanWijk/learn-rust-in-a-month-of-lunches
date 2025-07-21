1d8fb858181588a765c2cc05b73d10dc25dcdd2d
StephenVanWijk <stephenvanwijk6@gmail.com>
2025-07-21 16:58:58 +0200

    Paragraph 9.11 4 examples 20250721 1658CET SDvW

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,12 @@ struct Company {
 }
 
 impl Company {
+    
     fn new(name: &str, ceo: &str) -> Self {
         let ceo = match ceo {
             "" => None,
             ceo => Some(ceo.to_string()),
-        };
+        }; //
         Self {
             name: name.to_string(),
             ceo,
@@ -16,34 +17,16 @@ impl Company {
     }
 
     fn get_ceo(&self) -> Option<String> {
-        self.ceo.clone()
+        self.ceo.clone() //
     }
 }
+
 
 fn main() {
     mapping_and_filtering_paragraph_911_1();
     mapping_and_filtering_paragraph_911_2();
-
-    let user_input = vec![
-        "8.9",
-        "Nine point nine five",
-        "8.0",
-        "7.6",
-        "eleventy-twelve",
-    ];
-
-    let successful_numbers = user_input
-        .iter()
-        .filter_map(|input| input.parse::<f32>().ok())
-        .collect::<Vec<f32>>();
-
-    println!("{:?}", successful_numbers);
-
-    let x: Result<i32, &str> = Ok(-3);
-    assert_eq!(x.is_ok(), false);
-
-    let x: Result<i32, &str> = Err("Some error message");
-    assert_eq!(x.is_ok(), false);
+    mapping_and_filtering_paragraph_911_3();
+    mapping_andfiltering_paragraph_911_4();
 }
 
 fn mapping_and_filtering_paragraph_911_1() {
@@ -74,4 +57,45 @@ fn mapping_and_filtering_paragraph_911_2() {
         .collect();
 
     println!("{:?}", all_the_ceos);
+}
+
+fn mapping_and_filtering_paragraph_911_3(){
+    let user_input = vec![
+        "8.9",
+        "Nine point nine five",
+        "8.0",
+        "7.6",
+        "eleventy-twelve",
+    ];
+
+    let successful_numbers = user_input
+        .iter()
+        .filter_map(|input| input.parse::<f32>().ok())
+        .collect::<Vec<f32>>();
+
+    println!("{:?}", successful_numbers);
+
+    let x: Result<i32, &str> = Ok(-3);
+    assert_eq!(x.is_ok(), true);
+
+    let x: Result<i32, &str> = Err("Some error message");
+    assert_eq!(x.is_ok(), false);
+}
+
+fn mapping_andfiltering_paragraph_911_4(){
+    let company_vec = vec![
+        Company::new("Umbrella Corporation", "Unknown"),
+        Company::new("Ovintiv", "Brendan McCracken"),
+        Company::new("The Red-Headed League", ""),
+        Company::new("Stark Enterprises", ""),
+    ];
+
+    let results: Vec<Result<String, &str>> = company_vec
+        .iter()
+        .map(|company| company.get_ceo().ok_or("No CEO found"))
+        .collect();
+
+    for item in results {
+        println!("{:?}", item);
+    }
 }
