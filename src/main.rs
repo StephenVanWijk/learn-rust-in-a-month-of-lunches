fd192aec28bac312e60281bc4c9f88982779f5a5
StephenVanWijk <stephenvanwijk6@gmail.com>
2025-07-22 13:20:24 +0200

    Added chapter-nine 20250722 1320CET SDvW

--- src/main.rs
+++ src/main.rs
@@ -23,11 +23,14 @@ impl Company {
 }
 
 fn main() {
-    mapping_and_filtering_paragraph_911_1();
-    mapping_and_filtering_paragraph_911_2();
-    mapping_and_filtering_paragraph_911_3();
-    mapping_and_filtering_paragraph_911_4();
-    mapping_and_filtering_paragraph_911_5();
+    // mapping_and_filtering_paragraph_911_1();
+    // mapping_and_filtering_paragraph_911_2();
+    // mapping_and_filtering_paragraph_911_3();
+    // mapping_and_filtering_paragraph_911_4();
+    // mapping_and_filtering_paragraph_911_5();
+    mapping_and_filtering_paragraph_912_1();
+    mapping_and_filtering_paragraph_912_3();
+    // mapping_and_filtering_paragraph_913_1()
 }
 
 fn get_current_datetime() -> String{
@@ -133,3 +136,60 @@ fn mapping_and_filtering_paragraph_911_5(){
         .filter(|res| res.is_ok())
         .for_each(|res| println!("{:?}", res));
 }   
+
+fn in_char_vec(char_vec: &Vec<char>, check: char) {
+    println!(
+        "Is {check} inside? {}",
+        char_vec.iter().any(|&char| char == check)
+    );
+}
+
+fn mapping_and_filtering_paragraph_912_1(){
+    let num_array = ["8", "9", "Hi", "9898989898"];
+    let mut char_vec = vec![];
+
+    for index in 0..5 {
+        char_vec.push(
+            num_array
+                .get(index)
+                .and_then(|number| number.parse::<u64>().ok())
+                .and_then(|number| u64::try_from(number).ok()),
+        );
+    }
+
+    // Use filter_map to get only the Some values
+    let successful_values: Vec<u64> = char_vec
+        .into_iter()
+        .filter_map(|x| x)
+        .collect();
+    println!("{:?}", successful_values);
+}
+
+//20250722 Apparently you can substitute Opetion instances, Some(value) = true, None = false.
+fn mapping_and_filtering_paragraph_912_3(){
+    let try_1 = [Some("Okay!"), None, Some("Okay!"), Some("Okay!"), None];
+    let try_2 = [None, Some("Okay!"), Some("Okay!"), Some("Okay!"),Some("Okay!")];
+    let try_3 = [Some("Okay!"), Some("Okay!"), Some("Okay!"),Some("Okay!"), None];
+
+    for i in 0..try_1.len() {
+        println!("{:?}", try_1[i].and(try_2[i]).and(try_3[i]));
+    }
+}
+
+fn mapping_and_filtering_paragraph_913_1(){
+    let char_vec = ('A'..'頄').collect::<Vec<char>>();
+    in_char_vec(&char_vec, 'i');
+    in_char_vec(&char_vec, '삄');
+    in_char_vec(&char_vec, '鏗');
+
+    let smaller_vec = ('A'..'z').collect::<Vec<char>>();
+    print!("{:?}", smaller_vec);
+    println!(
+        "All alphabetic? {}",
+        smaller_vec.iter().all(|&x| x.is_alphabetic())
+    );
+    println!(
+        "All less than the character 행? {}",
+        smaller_vec.iter().all(|&x| x < '행')
+    );
+}
